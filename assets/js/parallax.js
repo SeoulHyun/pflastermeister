f8dc79ec71543d34502aba3aa941c15708f0a14f
Nino Hadwiger <ninohadwiger@yahoo.de>
2017-07-30 14:21:51 +0200

    Modified parallax configuration

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -3,7 +3,7 @@ $(window).scroll(function () {
     var wScroll = $(this).scrollTop();
 
     $('.title').css({
-        'transform' : 'translate(0px, ' + ((-wScroll / 2) % ($(window).height() / 2)) + '%)'
+        'transform' : 'translate(0px, ' + ((-wScroll / 1.5) % ($(window).height() / 2)) + '%)'
     })
 
 });
