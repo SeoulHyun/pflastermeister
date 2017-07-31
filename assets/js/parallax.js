f3c7d6d59e5f46397966db3c7752e456f8ff520d
Nino Hadwiger <ninohadwiger@yahoo.de>
2017-07-31 12:09:22 +0200

    - logo hover animation - contact form on click show

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,9 +1,15 @@
 $(window).scroll(function () {
+    $('#contact_form').hide()
 
     var wScroll = $(this).scrollTop();
 
+
     $('.title').css({
         'transform' : 'translate(0px, ' + ((-wScroll / 1.5) % ($(window).height() / 2)) + '%)'
+    })
+
+    $('#contact__link').click(function () {
+        $('#contact_form').show()
     })
 
 });
